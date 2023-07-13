a2e6a093fe1ffa5f586a8d8f275a2dc0aba8f452
TheRealAdegoke <adewaleadegoke0210@gmail.com>
2023-07-13 11:05:45 +0100

    We are live

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,44 +15,44 @@ router.post('/signup', async (req, res) => {
 
   // Check if all input fields are filled
   if (!username || !firstName || !lastName || !pin || !email || !password) {
-    return res.status(400).json({ error: 'Please fill in all fields' });
+    return res.status(400).send({ error: 'Please fill in all fields' });
   }
 
   // Check if the email is valid
   if (!emailRegex.test(email)) {
-    return res.status(400).json({ error: 'Invalid email address' });
+    return res.status(400).send({ error: 'Invalid email address' });
   }
 
   try {
     // Check if the username already exists
     const existingUsername = await User.findOne({ username });
     if (existingUsername) {
-      return res.status(400).json({ error: 'Username already exists' });
+      return res.status(400).send({ error: 'Username already exists' });
     }
 
     //Username regex
     if (username.length < 4 || username.length > 20) {
-        return res.status(400).json("Username must be between 2 and 20 characters long");
+        return res.status(400).send("Username must be between 2 and 20 characters long");
     }
 
     if (Number(username) || Number(username) === 0) {
-         return res.status(400).json("Username cannot contain only numbers");
+         return res.status(400).send("Username cannot contain only numbers");
     }
 
     const matchUsername = username.match(usernameRegex)
     if (!matchUsername) {
-        return res.status(400).json("Username can only contain letters and numbers");
+        return res.status(400).send("Username can only contain letters and numbers");
     }
 
     // Check if the email already exists
     const existingEmail = await User.findOne({ email });
     if (existingEmail) {
-      return res.status(400).json({ error: 'Email already exists' });
+      return res.status(400).send({ error: 'Email already exists' });
     }
 
     // Check if the password is at least 6 characters long
     if (password.length < 6) {
-      return res.status(400).json({ error: 'Password should be at least 6 characters long' });
+      return res.status(400).send({ error: 'Password should be at least 6 characters long' });
     }
 
     // Hash the password
@@ -70,10 +70,10 @@ router.post('/signup', async (req, res) => {
 
     // Save the user to the database
     await newUser.save();
-    res.status(200).json({ message: 'User registered successfully' });
+    res.status(200).send({ message: 'User registered successfully' });
   } catch (error) {
     console.error('Error registering user:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).send({ error: 'Internal server error' });
   }
 });
 
