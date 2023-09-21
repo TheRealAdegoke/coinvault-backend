ee6a7a136a750349f0c807006e49b1543638120c
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-09-21 12:45:24 +0100

    Coin route in progress

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cookieParser())
 app.use(express.json());
 
 app.use(cors({
-     origin: ['https://coinvault.onrender.com', 'https://www.google.com/',"http://localhost:5173", "http://localhost:8080"],
+     origin: ['https://coinvault.onrender.com', 'https://www.google.com/',"http://localhost:5173", "https://coin-vault.vercel.app/", "http://localhost:8080"],
      methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
