0bcf99f718c99052d5dba841ce0dc8848256256a
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-10-02 16:10:56 +0100

    Back to default

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const http = require("http");
-const socketIo = require("socket.io");
-const cookieParser = require("cookie-parser");
 const connectDB = require("./Database/connect");
 require("dotenv").config();
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const cookieParser = require("cookie-parser");
 
-const PORT = process.env.PORT || 8080;
-
-// Middleware
 app.use(cookieParser());
 app.use(express.json());
+
 app.use(
   cors({
     origin: [
@@ -30,55 +23,30 @@ app.use(
   })
 );
 
-// Additional CORS headers for Socket.io
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    'https://coinvault.onrender.com',
-      'https://www.google.com',
-      'http://localhost:5173',
-      'https://coin-vault.vercel.app',
-      'http://localhost:8080',
-      'http://192.168.43.251:5173',
-  ];
-
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, UPDATE, PUT, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
-  next();
-});
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("<h1>Giddy Up Soldier</h1>");
-});
-
 const authRoute = require("./routes/authRoutes");
 const userRoute = require("./routes/userRoutes");
 const coinRoute = require("./routes/coinRoutes");
 
+// API endpoint
+app.get("/", (req, res) => {
+  res.send("<h1>Giddy Up Soldier</h1>");
+});
+
 app.use("/", authRoute);
 app.use("/", userRoute);
+app.use("/", coinRoute);
 
-// WebSocket integration
-app.use("/", coinRoute(io));
+const port = process.env.PORT || 8080;
 
-// Start the server
-const startServer = async () => {
+const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    server.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}...`);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
     });
   } catch (error) {
-    console.error("Error starting server:", error);
+    console.log(error);
   }
 };
 
-startServer();
+start();
