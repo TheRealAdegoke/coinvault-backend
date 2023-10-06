9574650f4cc50ac0de58a6f994242082a6ff1cba
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-10-06 11:37:36 +0100

    here we go again

--- routes/coinRoutes.js
+++ routes/coinRoutes.js
@@ -8,7 +8,6 @@ const jwt = require("jsonwebtoken");
 const transactionHistoryModule = require("../Utils/transactionHistory");
 const notificationModule = require("../Utils/NotificationHistory");
 
-
 // Function to fetch cryptocurrency price from CoinGecko API
 async function getCryptoPrice(coinSymbol) {
   try {
@@ -17,26 +16,41 @@ async function getCryptoPrice(coinSymbol) {
     // Send a GET request to the CoinGecko API
     const response = await axios.get(apiUrl);
 
-    if (response.status === 200 && response.data[coinSymbol] && response.data[coinSymbol].usd) {
+    if (
+      response.status === 200 &&
+      response.data[coinSymbol] &&
+      response.data[coinSymbol].usd
+    ) {
       return response.data[coinSymbol].usd; // Return the USD price of the coin
     } else {
       throw new Error(`Failed to fetch cryptocurrency price for ${coinSymbol}`);
     }
   } catch (error) {
     console.error("Error fetching cryptocurrency price:", error.message);
-    throw new Error(`Failed to fetch cryptocurrency price for ${coinSymbol}: ${error.message}`);
+    throw new Error(
+      `Failed to fetch cryptocurrency price for ${coinSymbol}: ${error.message}`
+    );
   }
 }
 
 // Function to create transaction history
 async function createTransactionHistory(userId, status, message) {
-  return transactionHistoryModule.createTransactionHistory(userId, status, message);
+  return transactionHistoryModule.createTransactionHistory(
+    userId,
+    status,
+    message
+  );
 }
 
 // Route to buy cryptocurrency
 router.post("/v1/auth/buy-crypto", async (req, res) => {
   try {
     const { coinSymbol, amountToBuy } = req.body;
+
+    // Retrieve the userId from the authenticated user's JWT token
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.userId;
 
     // Ensure the amount to buy is greater than 0
     if (amountToBuy <= 0) {
@@ -45,18 +59,21 @@ router.post("/v1/auth/buy-crypto", async (req, res) => {
 
     // Ensure the amount to buy is greater than or equal to 50
     if (amountToBuy < 50) {
-      return res.status(400).send({ error: "Amount to buy must be at least 50 USD" });
+      // Log the failed transaction
+      await createTransactionHistory(
+        userId,
+        "failed",
+        `An unexpected error occurred while trying to purchase ${coinSymbol}`
+      );
+      return res
+        .status(400)
+        .send({ error: "Amount to buy must be at least 50 USD" });
     }
 
     // Check if the requested coin is in the allowed list
     if (!supportedCoins.includes(coinSymbol)) {
       return res.status(400).send({ error: "Coin not available for purchase" });
     }
-
-    // Retrieve the userId from the authenticated user's JWT token
-    const token = req.header("Authorization").replace("Bearer ", "");
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    const userId = decodedToken.userId;
 
     // Fetch user's wallet
     const wallet = await UserWallet.findOne({ userId });
@@ -69,14 +86,18 @@ router.post("/v1/auth/buy-crypto", async (req, res) => {
     const cryptoPrice = await getCryptoPrice(coinSymbol);
 
     if (!cryptoPrice) {
-      return res.status(400).send({ error: "Failed to fetch cryptocurrency price" });
+      return res
+        .status(400)
+        .send({ error: "Failed to fetch cryptocurrency price" });
     }
 
     // Calculate the equivalent amount of the cryptocurrency
     const equivalentAmount = amountToBuy / cryptoPrice;
 
     if (equivalentAmount <= 0) {
-      return res.status(400).send({ error: "Invalid amount or cryptocurrency" });
+      return res
+        .status(400)
+        .send({ error: "Invalid amount or cryptocurrency" });
     }
 
     // Check if the user already holds this cryptocurrency
@@ -90,8 +111,14 @@ router.post("/v1/auth/buy-crypto", async (req, res) => {
     // Check if the user has sufficient balance to make the purchase
     if (totalCost > wallet.balance) {
       // Log the failed transaction
-  await createTransactionHistory(userId, "failed", `An unexpected error occurred while trying to purchase ${coinSymbol}`);
-      return res.status(400).send({ error: "Insufficient balance to make the purchase" });
+      await createTransactionHistory(
+        userId,
+        "failed",
+        `An unexpected error occurred while trying to purchase ${coinSymbol}`
+      );
+      return res
+        .status(400)
+        .send({ error: "Insufficient balance to make the purchase" });
     }
 
     if (existingHoldingIndex !== -1) {
@@ -112,7 +139,11 @@ router.post("/v1/auth/buy-crypto", async (req, res) => {
     await wallet.save();
 
     // Log the buy transaction
-    await createTransactionHistory(userId, "successful", `Your Purchase of ${coinSymbol} was successful and ${amountToBuy} USD has been deducted from your account.`);
+    await createTransactionHistory(
+      userId,
+      "successful",
+      `Your Purchase of ${coinSymbol} was successful and ${amountToBuy} USD has been deducted from your account.`
+    );
 
     res.status(200).send({
       message: "Cryptocurrency purchased successfully",
@@ -153,18 +184,28 @@ router.post("/v1/auth/sell-crypto", async (req, res) => {
     }
 
     // Check if the user has the specified cryptocurrency in their wallet
-    const holding = wallet.cryptoHoldings.find((holding) => holding.coinSymbol === coinSymbol);
+    const holding = wallet.cryptoHoldings.find(
+      (holding) => holding.coinSymbol === coinSymbol
+    );
 
     if (!holding || holding.amount < amountToSell) {
-      await createTransactionHistory(userId, "failed", `An unexpected error occurred while trying to sell ${coinSymbol}`);
-      return res.status(400).send({ error: "Insufficient balance or no holdings for sale" });
+      await createTransactionHistory(
+        userId,
+        "failed",
+        `An unexpected error occurred while trying to sell ${coinSymbol}`
+      );
+      return res
+        .status(400)
+        .send({ error: "Insufficient balance or no holdings for sale" });
     }
 
     // Fetch the current price of the cryptocurrency (1 unit in USD)
     const cryptoPrice = await getCryptoPrice(coinSymbol);
 
     if (!cryptoPrice) {
-      return res.status(400).send({ error: "Failed to fetch cryptocurrency price" });
+      return res
+        .status(400)
+        .send({ error: "Failed to fetch cryptocurrency price" });
     }
 
     // Calculate the equivalent amount in USD for the sold cryptocurrency
@@ -178,15 +219,20 @@ router.post("/v1/auth/sell-crypto", async (req, res) => {
 
     // If the user has no more of this cryptocurrency, remove it from the holdings
     if (holding.amount === 0) {
-      wallet.cryptoHoldings = wallet.cryptoHoldings.filter((h) => h.coinSymbol !== coinSymbol);
+      wallet.cryptoHoldings = wallet.cryptoHoldings.filter(
+        (h) => h.coinSymbol !== coinSymbol
+      );
     }
 
     // Save the updated user document
     await wallet.save();
 
     // Log the sell transaction
-    await createTransactionHistory(userId, "successful", `You Successfully sold ${coinSymbol} and ${amountToSell} ${coinSymbol} has been deducted from your wallet`);
-
+    await createTransactionHistory(
+      userId,
+      "successful",
+      `You Successfully sold ${coinSymbol} and ${amountToSell} ${coinSymbol} has been deducted from your wallet`
+    );
 
     res.status(200).send({
       message: "Cryptocurrency sold successfully",
@@ -210,8 +256,13 @@ router.post("/v1/auth/swap-crypto", async (req, res) => {
     }
 
     // Check if both the fromCoin and toCoin are supported
-    if (!supportedCoins.includes(fromCoinSymbol) || !supportedCoins.includes(toCoinSymbol)) {
-      return res.status(400).send({ error: "One or both coins are not available for swapping" });
+    if (
+      !supportedCoins.includes(fromCoinSymbol) ||
+      !supportedCoins.includes(toCoinSymbol)
+    ) {
+      return res
+        .status(400)
+        .send({ error: "One or both coins are not available for swapping" });
     }
 
     // Retrieve the userId from the authenticated user's JWT token
@@ -227,11 +278,19 @@ router.post("/v1/auth/swap-crypto", async (req, res) => {
     }
 
     // Check if the user has the specified cryptocurrency in their wallet
-    const fromCoinHolding = wallet.cryptoHoldings.find((holding) => holding.coinSymbol === fromCoinSymbol);
+    const fromCoinHolding = wallet.cryptoHoldings.find(
+      (holding) => holding.coinSymbol === fromCoinSymbol
+    );
 
     if (!fromCoinHolding || fromCoinHolding.amount < amountToSwap) {
-      await createTransactionHistory(userId, "failed", `An unexpected error occurred while trying to swap ${fromCoinSymbol} to ${toCoinSymbol}`);
-      return res.status(400).send({ error: "Insufficient balance or no holdings for swapping" });
+      await createTransactionHistory(
+        userId,
+        "failed",
+        `An unexpected error occurred while trying to swap ${fromCoinSymbol} to ${toCoinSymbol}`
+      );
+      return res
+        .status(400)
+        .send({ error: "Insufficient balance or no holdings for swapping" });
     }
 
     // Fetch the current price of both the fromCoin and toCoin (1 unit in USD)
@@ -239,18 +298,23 @@ router.post("/v1/auth/swap-crypto", async (req, res) => {
     const toCoinPrice = await getCryptoPrice(toCoinSymbol);
 
     if (!fromCoinPrice || !toCoinPrice) {
-      return res.status(400).send({ error: "Failed to fetch cryptocurrency prices" });
+      return res
+        .status(400)
+        .send({ error: "Failed to fetch cryptocurrency prices" });
     }
 
     // Calculate the equivalent amount in USD for the fromCoin and toCoin
     const equivalentAmountInUSDFromCoin = amountToSwap * fromCoinPrice;
-    const equivalentAmountInUSDToCoin = equivalentAmountInUSDFromCoin / toCoinPrice;
+    const equivalentAmountInUSDToCoin =
+      equivalentAmountInUSDFromCoin / toCoinPrice;
 
     // Deduct the fromCoin from the user's holdings
     fromCoinHolding.amount -= amountToSwap;
 
     // Check if the user already holds the toCoin
-    const toCoinHolding = wallet.cryptoHoldings.find((holding) => holding.coinSymbol === toCoinSymbol);
+    const toCoinHolding = wallet.cryptoHoldings.find(
+      (holding) => holding.coinSymbol === toCoinSymbol
+    );
 
     if (toCoinHolding) {
       // If the user already holds the toCoin, update the amount
@@ -267,7 +331,11 @@ router.post("/v1/auth/swap-crypto", async (req, res) => {
     await wallet.save();
 
     // Log the swap transaction
-    await createTransactionHistory(userId, "successful", `You successfully Swapped ${amountToSwap} ${fromCoinSymbol} to ${toCoinSymbol}`);
+    await createTransactionHistory(
+      userId,
+      "successful",
+      `You successfully Swapped ${amountToSwap} ${fromCoinSymbol} to ${toCoinSymbol}`
+    );
 
     res.status(200).send({
       message: `Cryptocurrency swapped successfully from ${fromCoinSymbol} to ${toCoinSymbol}`,
@@ -283,7 +351,8 @@ router.post("/v1/auth/swap-crypto", async (req, res) => {
 // Route to transfer cryptocurrency for various coins using only receiver's address
 router.post("/v1/auth/transfer-crypto", async (req, res) => {
   try {
-    const { receiverCryptoCoinAddress, cryptoAmountToSend, CryptoToSend } = req.body;
+    const { receiverCryptoCoinAddress, cryptoAmountToSend, CryptoToSend } =
+      req.body;
 
     // Retrieve the userId from the authenticated user's JWT token
     const token = req.header("Authorization").replace("Bearer ", "");
@@ -310,14 +379,22 @@ router.post("/v1/auth/transfer-crypto", async (req, res) => {
       (holding) => holding.coinSymbol === CryptoToSend
     );
 
-    if (!senderCryptoHolding || senderCryptoHolding.amount < cryptoAmountToSend) {
-      await createTransactionHistory(senderUserId, "failed", `An unexpected error occurred while trying to transfer ${CryptoToSend} to ${receiverCryptoCoinAddress}`);
+    if (
+      !senderCryptoHolding ||
+      senderCryptoHolding.amount < cryptoAmountToSend
+    ) {
+      await createTransactionHistory(
+        senderUserId,
+        "failed",
+        `An unexpected error occurred while trying to transfer ${CryptoToSend} to ${receiverCryptoCoinAddress}`
+      );
       return res.status(400).send({ error: "Insufficient balance" });
     }
 
     // Find the receiver's wallet by matching the crypto address
     const receiverWallet = await UserWallet.findOne({
-      [`cryptoAddresses.${CryptoToSend.toLowerCase()}`]: receiverCryptoCoinAddress,
+      [`cryptoAddresses.${CryptoToSend.toLowerCase()}`]:
+        receiverCryptoCoinAddress,
     });
 
     if (!receiverWallet) {
@@ -326,7 +403,9 @@ router.post("/v1/auth/transfer-crypto", async (req, res) => {
 
     // Check if the receiver's address belongs to the sender
     if (receiverWallet.userId.equals(senderUserId)) {
-      return res.status(400).send({ error: "Can't send coins to your own address" });
+      return res
+        .status(400)
+        .send({ error: "Can't send coins to your own address" });
     }
 
     // Deduct the specified cryptocurrency from the sender's balance
@@ -349,15 +428,25 @@ router.post("/v1/auth/transfer-crypto", async (req, res) => {
     // Save the updated sender and receiver documents
     await senderWallet.save();
     await receiverWallet.save();
-    
+
     // Log the transfer transaction for the sender
-    await createTransactionHistory(senderUserId, "successful", `You Transferred ${cryptoAmountToSend} ${CryptoToSend} to ${receiverCryptoCoinAddress}. ${cryptoAmountToSend} ${CryptoToSend} has been deducted from your wallet`);
+    await createTransactionHistory(
+      senderUserId,
+      "successful",
+      `You Transferred ${cryptoAmountToSend} ${CryptoToSend} to ${receiverCryptoCoinAddress}. ${cryptoAmountToSend} ${CryptoToSend} has been deducted from your wallet`
+    );
 
     // Log the transfer transaction for the receiver
-    await createTransactionHistory(receiverWallet.userId, "received", `You Received ${cryptoAmountToSend} ${CryptoToSend} from ${sender.firstName} ${sender.lastName} and ${cryptoAmountToSend} ${CryptoToSend} has been added to your wallet`);
+    await createTransactionHistory(
+      receiverWallet.userId,
+      "received",
+      `You Received ${cryptoAmountToSend} ${CryptoToSend} from ${sender.firstName} ${sender.lastName} and ${cryptoAmountToSend} ${CryptoToSend} has been added to your wallet`
+    );
 
     res.status(200).send({
-      message: `Transferred ${cryptoAmountToSend.toFixed(4)} ${CryptoToSend} successfully`,
+      message: `Transferred ${cryptoAmountToSend.toFixed(
+        4
+      )} ${CryptoToSend} successfully`,
       senderBalance: senderWallet.balance,
       receiverBalance: receiverWallet.balance,
     });
@@ -371,7 +460,9 @@ router.post("/v1/auth/transfer-crypto", async (req, res) => {
 router.get("/v1/auth/transaction-history/:userId", async (req, res) => {
   try {
     const { userId } = req.params;
-    const transactions = await transactionHistoryModule.getTransactionHistory(userId);
+    const transactions = await transactionHistoryModule.getTransactionHistory(
+      userId
+    );
     res.status(200).json(transactions);
   } catch (error) {
     console.error("Error fetching transaction history:", error);
@@ -379,9 +470,8 @@ router.get("/v1/auth/transaction-history/:userId", async (req, res) => {
   }
 });
 
-
 // Route to fetch user's coin data
-router.get('/v1/auth/user-crypto-holdings/:userId', async (req, res) => {
+router.get("/v1/auth/user-crypto-holdings/:userId", async (req, res) => {
   try {
     const { userId } = req.params;
 
@@ -396,18 +486,24 @@ router.get('/v1/auth/user-crypto-holdings/:userId', async (req, res) => {
     const userCryptoData = [];
 
     // Fetch the current price and additional data of cryptocurrencies from CoinGecko
-    const coinSymbols = supportedCoins.join(',');
-    const coinInfoResponse = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSymbols}`);
+    const coinSymbols = supportedCoins.join(",");
+    const coinInfoResponse = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinSymbols}`
+    );
 
     if (coinInfoResponse.status !== 200) {
-      return res.status(400).json({ error: "Failed to fetch cryptocurrency information" });
+      return res
+        .status(400)
+        .json({ error: "Failed to fetch cryptocurrency information" });
     }
 
     const coinInfoData = coinInfoResponse.data;
 
     // Iterate through supported coins and add them to userCryptoData
     for (const coinSymbol of supportedCoins) {
-      const cryptoHolding = wallet.cryptoHoldings.find((holding) => holding.coinSymbol === coinSymbol);
+      const cryptoHolding = wallet.cryptoHoldings.find(
+        (holding) => holding.coinSymbol === coinSymbol
+      );
 
       // Calculate the equivalent amount
       let amount = 0;
@@ -420,24 +516,28 @@ router.get('/v1/auth/user-crypto-holdings/:userId', async (req, res) => {
       const coinInfo = coinInfoData.find((info) => info.id === coinSymbol);
 
       if (!coinInfo) {
-        return res.status(400).json({ error: `Coin information not found for ${coinSymbol}` });
+        return res
+          .status(400)
+          .json({ error: `Coin information not found for ${coinSymbol}` });
       }
 
       // Get the coin image URL from CoinGecko API
       const imageUrl = coinInfo.image;
 
       // Get the symbol from CoinGecko API
-      const symbol = coinInfo.symbol || '';
+      const symbol = coinInfo.symbol || "";
 
       // Get the 24h price change percentage from CoinGecko API
-      const price_change_percentage_24h = coinInfo.price_change_percentage_24h || 0;
+      const price_change_percentage_24h =
+        coinInfo.price_change_percentage_24h || 0;
 
       // Calculate the fiat value (worth in USD)
       const cryptoPriceInUSD = coinInfo.current_price; // The current price in USD from CoinGecko
       const fiatValue = amount * cryptoPriceInUSD;
 
       // Define the address based on your application's logic
-      const address = wallet.cryptoAddresses.get(coinSymbol.toLowerCase()) || '';
+      const address =
+        wallet.cryptoAddresses.get(coinSymbol.toLowerCase()) || "";
 
       // Create the user's crypto data object
       const cryptoData = {
@@ -485,6 +585,5 @@ router.put("/v1/auth/notifications/mark-as-read/:userId", async (req, res) => {
     res.status(500).send({ error: "Internal server error" });
   }
 });
-
 
 module.exports = router;
