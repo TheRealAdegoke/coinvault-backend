08d1127cb6e06d35d7c9e46545c8482f3aac4e47
Adegoke Gabriel Adewale <111113715+TheRealAdegoke@users.noreply.github.com>
2023-10-15 23:53:05 +0100

    trying vercel now

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -295,10 +295,7 @@ async function sendVerificationEmail(email, userName, verificationCode) {
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
+      }
     });
 
     const mailOptions = {
